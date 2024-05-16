bf4bfa8d04d20a304ab3e83c8f1130022e3df84b
Igor M <mporeigor@gmail.com>
2024-05-16 15:14:07 +0200

    Update: Add support for proper banner integration

--- widget-tag.js
+++ widget-tag.js
@@ -1,12 +1,19 @@
-console.log("[Noble] Noble script loaded 1.0.2");
+console.log("[Noble] Noble script loaded 1.0.3");
 
 window.addEventListener("message", function (event) {
 	let nobleIframe = document.getElementById("nobleIframe");
+	const allElements = document.querySelectorAll("*");
 
+	/**
+	 * Message to update iframe Height
+	 */
 	if (event.data.frameHeight) {
 		nobleIframe.style.height = event.data.frameHeight + `px`;
 	}
 
+	/**
+	 * Message to update iframe Width
+	 */
 	if (event.data.frameWidth) {
 		if (String(event.data.frameWidth).includes("%")) {
 			nobleIframe.style.width = event.data.frameWidth;
@@ -15,14 +22,72 @@ window.addEventListener("message", function (event) {
 		}
 	}
 
-	if (event.data === "bannerVisible") {
-		nobleIframe.style.bottom = "0px";
-		nobleIframe.style.top = "auto";
-		nobleIframe.style.left = "0px";
-	}
+	/**
+	 * Message to get URL where the widget is embedded
+	 */
 
 	if (event.data === "getURL") {
 		const iframeURL = window.location.href;
 		event.source.postMessage({ iframeURL: iframeURL }, event.origin);
+	}
+
+	/**
+	 * Message to:
+	 * - Change the positions of the top fixed/sticky elements
+	 * - Change the whole document body position
+	 */
+	if (event.data === "bannerVisible") {
+		nobleIframe.style.top = "0px";
+		nobleIframe.style.bottom = "auto";
+		nobleIframe.style.left = "0px";
+
+		//Move the body down
+		document.body.style.marginTop = "60px";
+
+		allElements.forEach((element) => {
+			// Ignore our iframe
+			if (element.id === "nobleIframe") return;
+
+			// Get the computed style of each element
+			const computedStyle = getComputedStyle(element);
+
+			// Check if the element is fixed or sticky
+			if (
+				computedStyle.position === "fixed" ||
+				computedStyle.position === "sticky"
+			) {
+				// Adjust the top position
+				const currentTop = parseInt(computedStyle.top) || 0;
+				element.style.top = currentTop + 60 + "px";
+			}
+		});
+	}
+
+	/**
+	 * Message to:
+	 * - Change the positions of the top fixed/sticky elements back to original
+	 * - Change the whole document body position  back to original
+	 */
+	if (event.data === "bannerMinimized") {
+		//Move the body back to 0
+		document.body.style.marginTop = "0";
+
+		allElements.forEach((element) => {
+			// Ignore our iframe
+			if (element.id === "nobleIframe") return;
+
+			// Get the computed style of each element
+			const computedStyle = getComputedStyle(element);
+
+			// Check if the element is fixed or sticky
+			if (
+				computedStyle.position === "fixed" ||
+				computedStyle.position === "sticky"
+			) {
+				// Adjust back the top position
+				const currentTop = parseInt(computedStyle.top) || 0;
+				element.style.top = currentTop - 60 + "px";
+			}
+		});
 	}
 });
