37ba8936bd6decf8830a648f1deeb6ba095e50ed
jcoll <colljames27@gmail.com>
2024-10-30 15:01:27 -0400

    first pass at revisons

--- widget-tag.js
+++ widget-tag.js
@@ -1,4 +1,4 @@
-console.log("[Noble] Noble script loaded 1.0.23");
+console.log("[Noble] Noble script loaded 1.0.24");
 
 let originalPositions;
 let wasIframeOnPrevPage; // boolean to indicate if header was already adjusted on previous navigation
@@ -42,9 +42,9 @@ const adjustPageContent = (isBannerVisible, nobleIframe, allElements) => {
 					element.style.top = currentTop + 116 + "px";
 				}
 			});
-		} else if (document.body.style.marginTop != "60px") {
+		} else if (document.body.style.marginTop != "100px") {
 			//Adjust desktop devices
-			nobleIframe.style.height = `60px`;
+			nobleIframe.style.height = `100px`;
 			allElements.forEach((element) => {
 				const computedStyle = getComputedStyle(element);
 
@@ -149,44 +149,32 @@ window.addEventListener("load", () => {
 	if (nobleIframe) {
 		wasIframeOnPrevPage = true;
 
-		/**
-		 * Function to compute the ORIGINAL positions
-		 * (top,bottom,right and left) of the iframe
-		 */
-		const getComputedStyleValue = (element, property) => {
-			const value = getComputedStyle(element).getPropertyValue(property);
+		//Adjust mobile devices
+		if (window.innerWidth < 640 && document.body.style.marginTop != "116px") {
+			nobleIframe.style.height = `116px`;
 
-			if (property === "top") {
-				let bottomValue = getComputedStyle(element).getPropertyValue("bottom");
-				return value < bottomValue ? value : "auto";
-			}
+			allElements.forEach((element) => {
+				const computedStyle = getComputedStyle(element);
 
-			if (property === "bottom") {
-				let topValue = getComputedStyle(element).getPropertyValue("top");
-				return value < topValue ? value : "auto";
-			}
+				if (element.id === "nobleIframe") return;
+				if (checkIsTopFixedElement(computedStyle)) {
+					const currentTop = parseInt(computedStyle.top) || 0;
+					element.style.top = currentTop + 116 + "px";
+				}
+			});
+		} else if (document.body.style.marginTop != "100px") {
+			//Adjust desktop devices
+			nobleIframe.style.height = `100px`;
+			allElements.forEach((element) => {
+				const computedStyle = getComputedStyle(element);
 
-			if (property === "left") {
-				let rightValue = getComputedStyle(element).getPropertyValue("right");
-				return value < rightValue ? value : "auto";
-			}
-
-			if (property === "right") {
-				let leftValue = getComputedStyle(element).getPropertyValue("left");
-				return value < leftValue ? value : "auto";
-			}
-		};
-
-		/**
-		 * Save original positions of the iframe
-		 * Use case: After maximizing the banner, the widget goes to these positions
-		 */
-		originalPositions = {
-			top: getComputedStyleValue(nobleIframe, "top"),
-			left: getComputedStyleValue(nobleIframe, "left"),
-			right: getComputedStyleValue(nobleIframe, "right"),
-			bottom: getComputedStyleValue(nobleIframe, "bottom"),
-		};
+				if (element.id === "nobleIframe") return;
+				if (checkIsTopFixedElement(computedStyle)) {
+					const currentTop = parseInt(computedStyle.top) || 0;
+					element.style.top = currentTop + 100 + "px";
+				}
+			});
+		}
 	}
 });
 
@@ -200,7 +188,7 @@ window.addEventListener("resize", () => iframeResize(false));
  */
 window.addEventListener("message", function (event) {
 	let nobleIframe = document.getElementById("nobleIframe");
-	const allElements = document.querySelectorAll("*");
+	
 	const trustedOrigins = [
 		"https://appdev.thatsnoble.com",
 		"https://app.thatsnoble.com"
@@ -227,23 +215,6 @@ window.addEventListener("message", function (event) {
 			}
 		}
 
-		/**
-		 * Message to update iframe Width
-		 */
-		if (event.data.frameWidth) {
-			if (String(event.data.frameWidth).includes("%")) {
-				nobleIframe.style.width = event.data.frameWidth;
-			} else {
-				if (window.innerWidth < 640) nobleIframe.style.width = `100%`;
-				else nobleIframe.style.width = event.data.frameWidth + `px`;
-			}
-
-			// Post the window width to the iframe
-			nobleIframe.contentWindow.postMessage(
-				{ windowWidth: window.innerWidth },
-				"*"
-			);
-		}
 
 		/**
 		 * Message to get URL where the widget is embedded
@@ -252,61 +223,6 @@ window.addEventListener("message", function (event) {
 		if (event.data === "getURL") {
 			const iframeURL = window.location.href;
 			event.source.postMessage({ iframeURL: iframeURL }, event.origin);
-		}
-
-		/**
-		 * Message to:
-		 * - Change the positions of the top fixed/sticky elements
-		 * - Change the whole document body position
-		 */
-		if (event.data.bannerVisibility === "bannerVisible") {
-			if (!event.data.bannerEmbed) {
-				wasBannerVisibleOnPrevPage = true;
-				nobleIframe.style.top = "0px";
-				nobleIframe.style.bottom = "auto";
-				nobleIframe.style.left = "0px";
-				iframeResize(true);
-
-				//Move the body down
-				if (window.innerWidth < 640) document.body.style.marginTop = "116px";
-				else document.body.style.marginTop = "60px";
-			} else {
-				iframeResize(false);
-				nobleIframe.style.margin = "32px auto";
-
-				if (window.innerWidth < 640) nobleIframe.style.height = `116px`;
-				else nobleIframe.style.height = `60px`;
-			}
-		}
-
-		/**
-		 * Message to:
-		 * - Change the positions of the top fixed/sticky elements back to original
-		 * - Change the whole document body position  back to original
-		 */
-		if (event.data.bannerVisibility === "bannerMinimized") {
-			nobleIframe.style.width = "0px";
-			nobleIframe.style.height = "0px";
-
-			if (!event.data.bannerEmbed) {
-				wasBannerVisibleOnPrevPage = false;
-				//Move the body back to 0 and remove the iframe
-				document.body.style.marginTop = "0";
-				nobleIframe.style.top = originalPositions
-					? originalPositions.top
-					: "80px";
-				nobleIframe.style.bottom = originalPositions
-					? originalPositions.bottom
-					: "auto";
-				nobleIframe.style.left = originalPositions
-					? originalPositions.left
-					: "0px";
-				nobleIframe.style.right = originalPositions
-					? originalPositions.right
-					: "auto";
-
-				adjustPageContent(false, null, allElements);
-			} else nobleIframe.style.margin = "32px auto";
 		}
 
 		/**
@@ -325,6 +241,6 @@ window.addEventListener("message", function (event) {
 		}
 	} else {
 		// Log the case where the origin is not trusted
-		// console.warn("Message received from an untrusted origin:", event.origin);
+		console.warn("Message received from an untrusted origin:", event.origin);
 	}
 });
